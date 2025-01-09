eb11ad830b6b96f346d074fec26e775460f77621
rajashekhar-a <rajuavunuri123@gmail.com>
2025-01-09 22:25:31 +0530

    add variables

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -9,4 +9,7 @@ module "shipping" {
   PORT                = 8080
   IS_PRIVATE_LB       = "true"
   LB_RULE_PRIORITY    = 104
+  APP_VERSION         = var.APP_VERSION
+  NEXUS_USER          = var.NEXUS_USER
+  NEXUS_PASS          = var.NEXUS_PASS
 }
